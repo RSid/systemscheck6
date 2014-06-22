2257ad65f14ec6cac0b57a0fee9283c435fe6259
RSid <sm1th303@gmail.com>
2014-06-22 13:35:17 -0400

    path helpers link from restaurant to new review for restaurant form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,12 @@
 class ReviewsController < ApplicationController
-  def index
-
-  end
-
-  def show
-
-  end
 
   def new
-    #binding.pry
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new()
   end
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    binding.pry
     @review = Review.new(body: params['review']['body'], rating: params['review']['rating'],restaurant_id: params['restaurant_id'])
 
     if @review.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:index, :show, :new, :create]
+    resources :reviews, only: [:new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
